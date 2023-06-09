5e312cd91759f782188ac0ec86b032b3d97cb6ed
Naveen M K <naveen521kk@gmail.com>
2023-06-09 21:45:05 +0530

    Set rich display for VMobject/VGroup on Jupyter
    This should allow to display a VMobject/VGroup directly on a notebook.

--- src/manim_mobject_svg/__init__.py
+++ src/manim_mobject_svg/__init__.py
@@ -5,3 +5,7 @@ __all__ = ["create_svg_from_vgroup", "create_svg_from_vmobject"]
 
 VMobject.to_svg = create_svg_from_vmobject
 VGroup.to_svg = create_svg_from_vgroup
+
+# Set rich display for manim objects on Jupyter
+VMobject._repr_svg_ = lambda self: create_svg_from_vmobject(self).read_text()
+VGroup._repr_svg_ = lambda self: create_svg_from_vgroup(self).read_text()
